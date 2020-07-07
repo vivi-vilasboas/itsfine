43bdc5e6b35f1b0d4b85177548bb062f8c849b54
elaineestainer <elaineestainer@yahoo.com.br>
2020-07-07 11:06:05 -0300

    Inserido contador regressivo e alterado o logo

---
+++ src/js/script.js
@@ -0,0 +1,42 @@
+function contador(){
+    let now = new Date();
+    let eventDate = new Date("July 09, 2020 20:00:00");
+  
+    let currentTime = now.getTime();
+    let eventTime = eventDate.getTime();
+  
+    let remTime = eventTime - currentTime;
+  
+    let seg = Math.floor(remTime / 1000);
+    let min = Math.floor(seg / 60);
+    let hora = Math.floor(min / 60);
+    let dia = Math.floor(hora / 24);
+  
+    hora %= 24;
+    min %= 60;
+    seg %= 60;
+  
+    if (dia <= 0 && hora <= 0 && min <= 0 && seg <= 0) {
+      
+      dia = '-'
+      hora = '--'
+      min = '--'
+      seg = '--'
+    }
+    
+    hora = (hora < 10) ? "0" + hora : hora;
+    min = (min < 10) ? "0" + min : min;
+    seg = (seg < 10) ? "0" + seg : seg;
+  
+    document.getElementById("days").textContent = dia;
+    document.getElementById("days").innerText = dia;
+  
+    document.getElementById("hours").textContent = hora;
+    document.getElementById("minutes").textContent = min;
+    document.getElementById("seconds").textContent = seg;  
+  
+    setTimeout(contador, 1000);
+    
+  }
+  
+  contador()
